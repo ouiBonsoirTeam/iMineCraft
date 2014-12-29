9cd7fdb06c494055f8d64adf278a6e72562987fd
MarionAudebert <marion.audebert@free.fr>
2014-12-29 17:55:34 +0100

    test obj ok
    missing .obj and .mtl in the bin

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <glimac/CustomProgram.hpp>
 #include <glimac/FreeFlyCamera.hpp>
 #include <glimac/Torch.hpp>
+#include <glimac/Geometry.hpp>
 
 #include "voxel_engine/Chunk.hpp"
 #include "physics/Event_manager.hpp"
@@ -121,6 +122,93 @@ int main(int argc, char** argv) {
 	Skybox skybox;
 	skybox.init(skyProg);
 
+	Geometry geo;
+
+	//load obj
+	if (!geo.loadOBJ("bin/assets/obj/hairball.obj", "bin/assets/obj/mitsuba.mtl", true))
+		std::cerr << "Impossible de charger l'objet" << std::endl;
+
+	std::cerr << "tortue" << std::endl;
+
+	//load texture
+	std::unique_ptr<Image> texturePointer2;
+	texturePointer2 = loadImage("../iMineCraft/assets/textures/default.png");
+	if(texturePointer2 == NULL)
+	{
+		std::cerr << "Error while charging texture." << std::endl;
+	}
+
+	std::cerr << "dindon" << std::endl;
+
+	GLuint idTexture2;
+	glGenTextures(1, &idTexture2);
+	glBindTexture(GL_TEXTURE_2D,  idTexture2);
+	glTexImage2D(GL_TEXTURE_2D,  0,  GL_RGBA,  texturePointer2->getWidth(),  
+					texturePointer2->getHeight(),  0,  GL_RGBA,  GL_FLOAT,  texturePointer2->getPixels());
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D,  0);
+
+	std::cerr << "koala" << std::endl;
+
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glBufferData(GL_ARRAY_BUFFER, geo.getVertexCount() * sizeof(glm::vec3), geo.getVertexBuffer(), GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	std::cerr << "bison" << std::endl;
+
+	GLuint ibo;
+    glGenBuffers(1, &ibo);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, geo.getIndexCount() * sizeof(uint32_t), geo.getIndexBuffer(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
+
+
+	const static GLuint VERTEX_ATTR_POSITION = 0;
+	const static GLuint VERTEX_ATTR_NORMAL = 1;
+	const static GLuint VERTEX_ATTR_TEXTCOORD = 2;
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+
+	glBindVertexArray(vao);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+
+		glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+		glEnableVertexAttribArray(VERTEX_ATTR_NORMAL);
+		glEnableVertexAttribArray(VERTEX_ATTR_TEXTCOORD);
+
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+			glVertexAttribPointer(VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(Geometry::Vertex), (const GLvoid*)(0* sizeof(GLfloat)));
+			glVertexAttribPointer(VERTEX_ATTR_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(Geometry::Vertex), (const GLvoid*)(3 * sizeof(GLfloat)));
+			glVertexAttribPointer(VERTEX_ATTR_TEXTCOORD, 2, GL_FLOAT, GL_FALSE, sizeof(Geometry::Vertex), (const GLvoid*)(6 * sizeof(GLfloat)));
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
+
+	std::cerr << "lapin" << std::endl;
+
+
+
+
+
 
 	// // make me a torch
 	// Torch torch;
@@ -148,7 +236,7 @@ int main(int argc, char** argv) {
 		//std::cout<<"diff : "<< (1.f/max_fps) - (currentTime - lastTime2) << std::endl;
 		if (currentTime - lastTime2 < (1.f/max_fps) && currentTime - lastTime2 > 0)
 		{
-			usleep( (unsigned int)(((1.f/max_fps) - (currentTime - lastTime2))*2000000) ) ;
+			//usleep( (unsigned int)(((1.f/max_fps) - (currentTime - lastTime2))*2000000) ) ;
 			//std::cout<<"zizi"<<std::endl;
 		}
 		lastTime2 = currentTime;
@@ -171,12 +259,48 @@ int main(int argc, char** argv) {
 		chunk.render(gProgram, viewMatrix, idTexture);
 
 
+		//render pour l'obj
+			glm::mat4 modelMatrix = glm::translate(glm::mat4(1.f), glm::vec3(12,3,12));
+			glm::mat4 modelViewMatrix = viewMatrix * modelMatrix;
+
+			// A sortir de la classe : Identique dans tout le programme
+			glm::mat4 projMatrix = glm::perspective(glm::radians(70.f), 800.f/600.f, 0.1f, 100.f);
+
+			glm::mat4 modelViewProjMatrix = projMatrix * modelViewMatrix;
+
+			// Normale
+			glm::mat4 normalMatrix = glm::transpose(glm::inverse(modelViewMatrix));
+
+			glUniformMatrix4fv(gProgram.uMVMatrix, 1, GL_FALSE, glm::value_ptr(modelViewMatrix));
+			glUniformMatrix4fv(gProgram.uMVPMatrix, 1, GL_FALSE, glm::value_ptr(modelViewProjMatrix));
+			glUniformMatrix4fv(gProgram.uNormalMatrix, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+			glBindVertexArray(vao);
+
+			glBindTexture(GL_TEXTURE_2D, idTexture2);
+			glUniform1i(gProgram.uTexture, 0);
+
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+
+			glDrawElements(GL_TRIANGLES, geo.getIndexCount(), GL_UNSIGNED_INT, 0);
+
+			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+
+			glBindTexture(GL_TEXTURE_2D, 0);
+
+			glBindVertexArray(0);
+
+
 		// Update the display
 		windowManager.swapBuffers();
 
 	}
 
 	skybox.destruct();
+	glDeleteBuffers(1, &vbo);
+	glDeleteVertexArrays(1, &vao);
 
 	return EXIT_SUCCESS;
 }
